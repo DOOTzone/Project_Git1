07b9e382e61e60dac61f90ecf53426b1c531de57
Jan Polách <polachj.05@spst.cz>
2022-02-09 08:53:07 +0100

    Faktoraiál

--- Project Git1/Form1.cs
+++ Project Git1/Form1.cs
@@ -16,5 +16,16 @@ namespace Project_Git1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            int fak = 1;
+            for(int z = 2; z <= x; z++)
+            {
+                fak *= z;
+            }
+            MessageBox.Show("Faktoriál čísla " + x + " je " + fak,"Faktoriál",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
     }
 }
